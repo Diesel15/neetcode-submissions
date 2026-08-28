a80c3956d7a054db3cd66b123726e3e2475ba345
Diesel15 <90098746+Diesel15@users.noreply.github.com>
2026-08-28 22:35:06 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 5.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'Hell'
+print(type(variable))
+variable = [1,2,3,4]
+print(type(variable))
